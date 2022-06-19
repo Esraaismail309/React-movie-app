8aa6e51a3c10b3d10161b15393540c8bef7aecac
Esraa Ismail <esraaismail309@gmail.com>
2022-06-19 18:32:26 +0200

    handle search form

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -5,22 +5,25 @@ import { Search } from './Search'
 const Navbar = () => {
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top py-3">
             <div className="container">
                 <Link to='/movie-app/popularmovies/1' className="navbar-brand fw-bolder" >Movie App</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
                         <li className="nav-item">
                             <Link to='/movie-app/popularmovies/1' className="nav-link active" aria-current="page" >Popular</Link >
                         </li>
                         <li className="nav-item">
                             <Link to={'/movie-app/wishlist'} className="nav-link" >Wishlist</Link>
                         </li>
+                        <li>
+                            <Search />
+                        </li>
                     </ul>
-                    <Search />
+
                 </div>
             </div>
         </nav>
--- src/components/shared/Search.js
+++ src/components/shared/Search.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { FaLink, FaLongArrowAltLeft, FaImdb } from "react-icons/fa";
+
 import { fetchFilterdMoviesRequest } from "../../redux/searchMovie/SearchActions";
 
 export const Search = () => {
@@ -10,24 +12,48 @@ export const Search = () => {
   const onClickHandler = (e) => {
     e.preventDefault()
     dispatch(fetchFilterdMoviesRequest(1, userInputSearch))
+
+  }
+
+  const resetFun = () => {
     setUserInputSearch('')
   }
   return (
-    <div>
-      <form onSubmit={(e) => onClickHandler(e)}>
-        <input
-          type="text"
-          className="border border-2 rounded me-2 mt-2 px-3 py-2 w-75"
-          value={userInputSearch}
-          onChange={(e) => { setUserInputSearch(e.target.value) }}
-          placeholder='search movie ...'
-        />
-        <button type="submit" className="btn bg-dark mb-2">
-          <Link to={`/movie-app/filterdmovie/1/${userInputSearch}`}>
-            <FiSearch className="text-white my-1 " />
+
+    <form onSubmit={(e) => {
+      onClickHandler(e)
+
+    }}
+      className='d-flex'>
+      <input
+        type="text"
+        className="border border-2 rounded me-2 px-3 w-75"
+        value={userInputSearch}
+        onChange={(e) => { setUserInputSearch(e.target.value) }}
+        placeholder='search movie ...'
+      />
+      {userInputSearch !== '' ? (
+        <>
+          <button type="submit" className="btn bg-dark mb-2 py-2">
+            <Link to={`/movie-app/filterdmovie/1/${userInputSearch}`}>
+              <FiSearch className="text-white my-1 " />
+            </Link>
+          </button>
+          <Link
+            to={`/movie-app/popularmovies/1`}
+            className="btn btn-dark border shadow  border-dark ms-2 col-4 py-2 h-100 "
+            onClick={resetFun}
+          >
+            <FaLongArrowAltLeft /> Back Home
           </Link>
-        </button>
-      </form>
-    </div>
+        </>
+      ) : <button type="submit" className="btn bg-dark disabled py-2">
+        <Link to={`/movie-app/filterdmovie/1/${userInputSearch}`}>
+          <FiSearch className="text-white my-1 " />
+        </Link>
+      </button>}
+
+
+    </form>
   );
 };
