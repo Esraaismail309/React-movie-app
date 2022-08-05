c88ce8c9ec3e88766af023f1653a137646fa1547
Esraa Ismail <esraaismail309@gmail.com>
2022-08-05 23:53:19 +0200

    handle bug

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Wrapper>
-      <HashRouter>
+    <HashRouter>
+      <Wrapper>
         <App />
-      </HashRouter>
-    </Wrapper>
+      </Wrapper>
+    </HashRouter>
   </React.StrictMode >
 );
 
